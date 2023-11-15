98988117270b6bf1873b651617a4a87856f9d6cf
sunny <sunnyrockzzz1811@gmail.com>
2023-11-15 14:15:25 +0530

    Bug Fix

--- code_editor.py
+++ code_editor.py
@@ -6,10 +6,13 @@ from PyQt6.QtCore import Qt
 class CodeEditor(QTextEdit):
     def __init__(self):
         super().__init__()
+
+        # Style
         self.setStyleSheet("""
                         background-color: #1E1E1E;
                         color: #F0F0F0;
                         border: 2px solid #505050;
+                        font-family: Arial;
                         font-size: 20px;
                     }""")
 
@@ -22,20 +25,24 @@ class CodeEditor(QTextEdit):
             cursor = self.textCursor()
             line_text = cursor.block().text().strip()
             self.insertPlainText("\n")
-
             try:
                 if line_text[-1] == ":":
                     self.insertPlainText("     ")
+                    self.to_indent = True
             except:
                 pass
 
+
         elif event.key() == Qt.Key.Key_ParenLeft:
             self.insertPlainText("()")
-            self.moveCursor(self.textCursor().MoveOperation.Left)
+            self.move_cursor_left()
 
         elif event.key() == Qt.Key.Key_QuoteDbl:
             self.insertPlainText("\"\"")
-            self.moveCursor(self.textCursor().MoveOperation.Left)
+            self.move_cursor_left()
 
         else:
             super().keyPressEvent(event)
+
+    def move_cursor_left(self):
+        self.moveCursor(self.textCursor().MoveOperation.Left)
